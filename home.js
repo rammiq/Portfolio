3764aebd229150dbe6171a12ced1ce8bb82c21b9
Ramon <94244415+rammiq@users.noreply.github.com>
2022-01-03 21:25:12 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,247 @@
+const line = document.querySelector(".timeline-innerline");
+
+let i = 0;
+let i2 = 1;
+let target1 = document.querySelector(".timeline ul");
+let target2 = document.querySelectorAll(".timeline ul li");
+
+const timeline_events = document.querySelectorAll("ul li");
+
+function showTime(e) {
+  e.setAttribute("done", "true");
+  e.querySelector(".timeline-point").style.background = "blue";
+  e.querySelector(".date").style.opacity = "100%";
+  e.querySelector("p").style.opacity = "100%";
+  e.querySelector("p").style.transform = "translateY(0px)";
+}
+
+function hideTime(e) {
+  e.removeAttribute("done");
+  e.querySelector(".timeline-point").style.background = "rgb(228, 228, 228)";
+  e.querySelector(".date").style.opacity = "0%";
+  e.querySelector("p").style.opacity = "0%";
+  e.querySelector("p").style.transform = "translateY(-10px)";
+}
+
+function slowLoop() {
+  setTimeout(function () {
+    showTime(timeline_events[i]);
+    timelineProgress(i + 1);
+    i++;
+    if (i < timeline_events.length) {
+      slowLoop();
+    }
+  }, 800);
+}
+
+
+function timelineProgress(value) {
+  let progress = `${(value / timeline_events.length) * 100}%`;
+  if (window.matchMedia("(min-width: 728px)").matches) {
+    line.style.width = progress;
+    line.style.height = "4px";
+  } else {
+    line.style.height = progress;
+    line.style.width = "4px";
+  }
+}
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0.9) {
+        if (window.matchMedia("(min-width: 728px)").matches) {
+          slowLoop();
+        } else {
+          showTime(entry.target);
+          timelineProgress(i2);
+          i2++;
+        }
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 1, rootMargin: "0px 0px -50px 0px" }
+);
+
+if (window.matchMedia("(min-width: 728px)").matches) {
+  observer.observe(target1);
+} else {
+  target2.forEach((t) => {
+    observer.observe(t);
+  });
+}
+
+
+timeline_events.forEach((li, index) => {
+  li.addEventListener("click", () => {
+    if (li.getAttribute("done")) {
+      timelineProgress(index);
+
+      // hide all timeline events from last upto the point clicked
+      timeline_events.forEach((ev, idx) => {
+        if (idx >= index) {
+          hideTime(ev);
+        }
+      });
+    } else {
+      timelineProgress(index + 1);
+      // show all timeline events from first upto the point clicked
+      timeline_events.forEach((ev, idx) => {
+        if (idx <= index) {
+          showTime(ev);
+        }
+      });
+    }
+  });
+});
+
+var doit;
+window.addEventListener("resize", () => {
+  clearTimeout(doit);
+  doit = setTimeout(resizeEnd, 1200);
+});
+
+function resizeEnd() {
+  i = 0;
+  slowLoop();
+}
+
+
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
+
+function init() {
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = 'none';
+
+    main.style.display = 'block';
+    setTimeout(() => (main.style.opacity = 1), 50);
+  }, 4000);
+}
+
+init();
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
+function debounce(func, wait = 20, immediate = true) {
+	var timeout;
+	return function() {
+		var context = this,
+			args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+	sliderImages.forEach(sliderImage => {
+		// halfway through the image
+		const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+		// bottom of the image
+		const imageBottom = sliderImage.offsetTop + sliderImage.height;
+		const isHalfShown = slideInAt > sliderImage.offsetTop;
+		const isNotScrolledPast = window.scrollY < imageBottom;
+		if (isHalfShown && isNotScrolledPast) {
+			sliderImage.classList.add('active');
+		} else {
+			sliderImage.classList.remove('active');
+		}
+
+		console.log(slideInAt);
+	});
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
+
+
